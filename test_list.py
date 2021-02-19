f9edadef6d41376e911c40fb6b08219cfd526e04
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-02-19 23:42:12 +0200

    add test for swap

--- test_list.py
+++ test_list.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class TestListMethods(unittest.TestCase):
+	def test_liubov_peleshenko_fi_94(self):
+		list [1, 2, 3, 4, 5, 6]
+		self.assertEqual(swap_first_last_elements(list),[6, 2, 3, 4, 5, 1])
 
-    def test_roman_tkalenko_fi_13(self):
+    def test_liubov_peleshenko_fi_94(self):
         self.assertEqual(len([]), 0)
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
